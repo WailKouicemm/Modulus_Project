6c11ee6d8e5964179c426dd2b16b7646f3b3b80c
WailKouicemm <wailkouicem0@gmail.com>
2023-05-05 00:53:35 +0100

    add evenement and commentaire field to Lieu_admin_page / commentaire for User_admin_page

--- mainbdd/admin.py
+++ mainbdd/admin.py
@@ -6,18 +6,29 @@ from .models import *
 class HoraireInline(admin.TabularInline):
     model = Horaire
     extra = 1
+class CommentaireInline(admin.TabularInline):
+    model = Commentaire
+    extra = 1
+
+class EvenementInline(admin.TabularInline):
+    model = Evenement
+    extra = 1
+
 
 class LieuAdmin(admin.ModelAdmin):
-    inlines = [HoraireInline]
+    inlines = [HoraireInline ,EvenementInline,CommentaireInline ]
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [CommentaireInline ]
     
     
 
 
-admin.site.register(User)
+admin.site.register(User , UserAdmin)
 admin.site.register(Lieu , LieuAdmin)
-admin.site.register(Categorie)
-admin.site.register(Theme)
+#admin.site.register(Categorie)
+#admin.site.register(Theme)
 #admin.site.register(Horaire)
-admin.site.register(Transport)
-admin.site.register(Commentaire)
-admin.site.register(Evenement)
+#admin.site.register(Transport)
+#admin.site.register(Commentaire)
+#admin.site.register(Evenement)
