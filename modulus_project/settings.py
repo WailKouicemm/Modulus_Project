61e82da04d6e3141f9dcf4f4bff6f9c8206a4f84
WailKouicemm <wailkouicem0@gmail.com>
2023-05-06 21:36:54 +0100

    modify access token lifetime

--- modulus_project/settings.py
+++ modulus_project/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=360),
 }
 
 DJOSER = {
